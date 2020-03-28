d51d474a2742f20b1aeccaaabd3b56a1b7849d42
wangtao0101 <yuecjn@gmail.com>
2020-03-28 21:42:06 +0800

    feat: normalize config fro apollo and nacos

--- package/base/app/app.py
+++ package/base/app/app.py
@@ -10,14 +10,20 @@ webapp = Flask(__name__)
 
 commonBp = Blueprint('kpi', __name__)
 
-SERVER_ADDRESSES = "127.0.0.1:8848"
-NAMESPACE = "public"
-SERVICE_NAME = "python.test.domain"
+NACOS_SERVER_ADDRESSES = "127.0.0.1:8848"
+NACOS_NAMESPACE = "public"
+NACOS_SERVICE_NAME = "python.test.domain"
+NACOS_INSTANCE_IP = "127.0.0.1"
+NACOS_INSTANCE_PORT = "5000"
 
 
-nacosClient = NacosClient(SERVER_ADDRESSES, SERVICE_NAME, NAMESPACE)
+APOLLO_APP_ID = "SampleApp"
+APOLLO_SERVER_URL = "http://localhost:8080"
+
+nacosClient = NacosClient(NACOS_SERVER_ADDRESSES, NACOS_SERVICE_NAME,
+                          NACOS_INSTANCE_IP, NACOS_INSTANCE_PORT, NACOS_NAMESPACE)
 apolloClient = ApolloClient(
-    app_id="SampleApp", config_server_url="http://localhost:8080")
+    app_id=APOLLO_APP_ID, config_server_url=APOLLO_SERVER_URL)
 
 # 暂时不需要start
 # apolloClient.start()
--- package/base/nacos/client.py
+++ package/base/nacos/client.py
@@ -13,10 +13,12 @@ SERVICE_INFO_SPLITER = '@@'
 
 
 class NacosClient:
-    def __init__(self, server_addresses, service_name, namespace=None, ):
+    def __init__(self, server_addresses, service_name, instance_ip, instance_port, namespace=None):
         self.server_addresses = server_addresses
         self.service_name = DEFAULT_GROUP + SERVICE_INFO_SPLITER + service_name
         self.namespace = namespace
+        self.instance_ip = instance_ip
+        self.instance_port = instance_port
 
         self.connect()
 
@@ -24,11 +26,11 @@ class NacosClient:
         self.client = nacos.NacosClient(
             self.server_addresses, namespace=self.namespace)
         self.client.add_naming_instance(
-            self.service_name, "127.0.0.1", "5000", None, 1.0, metadata={})
+            self.service_name, self.instance_ip, self.instance_port, None, 1.0, metadata={})
         self.beat()
 
     def beat(self):
-        self.client.send_heartbeat(self.service_name, "127.0.0.1",
+        self.client.send_heartbeat(self.service_name, self.instance_ip,
                                    "5000", None, 1.0, "{}")
         Timer(4, self.beat, ()).start()
 
